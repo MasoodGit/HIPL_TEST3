4d7ed95fbb088fd8c8ba8bbe06c355bc96fbdcc5
Masood_Alam <mohamed.masood.alam@philips.com>
2016-11-06 22:35:16 +0530

    test new ssh-key

--- MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
+++ MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
@@ -10,7 +10,53 @@ namespace MyFirstConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            Console.WriteLine("Enter a number : ");
+
+            int number = int.Parse(Console.ReadLine());
+
+            string suffix = getOrdinal(number);
+
+            Console.WriteLine("Thats the {0}-{1} item", number, suffix);
+
+            Console.ReadLine();
+        }
+
+        public static int getRightMostDigit(int number)
+        {
+            return number % 10;
+        }
+
+        public static string getOrdinal(int number)
+        {
+
+            string suffix = "th";
+
+            int rightMostDigit = getRightMostDigit(number);
+
+            if (rightMostDigit == 1 || rightMostDigit == 2 || rightMostDigit == 3)
+            {
+
+                int cardinal = number % 100;
+                if (cardinal == 11 || cardinal == 12 || cardinal == 13)
+                {
+                    return suffix;
+                }
+            }
+
+            if (rightMostDigit == 1)
+            {
+                suffix = "st";
+            }
+            else if (rightMostDigit == 2)
+            {
+                suffix = "nd";
+            }
+            else if (rightMostDigit == 3)
+            {
+                suffix = "rd";
+            }
+
+            return suffix;
         }
     }
 }
